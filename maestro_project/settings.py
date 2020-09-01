6352e5174268599ed001739dea46635a8ad9a6db
Ishaq Ahmad Nizhami <Nizhami@Nizhamis-MacBook-Air.local>
2020-09-01 10:37:12 +0700

    bug email

--- maestro_project/settings.py
+++ maestro_project/settings.py
@@ -145,7 +145,7 @@ LOGIN_REDIRECT_URL = 'index'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'urbanstadia@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'ijamandyX46'
 EMAIL_USE_TLS = True
 
 # Heroku: Update database configuration from $DATABASE_URL.
